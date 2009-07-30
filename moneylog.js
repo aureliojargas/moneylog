edc702273839017e969b01a37bfcf8f321c869bf
Aurelio Jargas <verde@aurelio.net>
2009-07-30 21:46:50 +0000

    Troquei == por === pra deixar o JSLint feliz.

--- moneylog.js
+++ moneylog.js
@@ -174,7 +174,7 @@ function addMonths(yyyymmdd, n) {
 	if (m > 12) {
 		y = y + Math.floor(m / 12);
 		m = m % 12;
-		if (m == 0) { // Exception for n=24, n=36, ...
+		if (m === 0) { // Exception for n=24, n=36, ...
 			m = 12;
 			y = y - 1;
 		}
