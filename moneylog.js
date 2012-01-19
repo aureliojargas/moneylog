99000d7ec64e5a2df54c16660187c7fea7036f80
Aurelio Jargas <verde@aurelio.net>
2012-01-19 17:15:23 +0000

    New config dataFilesDefault to specify the default selected file at startup when using multiple TXT files (local or Dropbox);

--- moneylog.js
+++ moneylog.js
@@ -46,6 +46,7 @@ var initChartYearly = 1;          // Initial selected item for the yearly chart 
 // Program structure and files
 var oneFile = false;              // Full app is at moneylog.html single file?
 var dataFiles = ['moneylog.txt']; // The paths for the data files (requires oneFile=false)
+var dataFilesDefault = '';        // Default selected file at init when using multiple TXT
 
 // localStorage allows editing in-place, saving your data on the browser (like cookies)
 var useLocalStorage = false;      // Turn ON localStorage support?
@@ -479,6 +480,39 @@ Array.prototype.removePattern = function (patt, n) { // n = number of removes
 	return cleaned;
 };
 
+// https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf
+if (!Array.prototype.indexOf) {  
+	Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {	
+		"use strict";  
+		if (this == null) {	 
+			throw new TypeError();	
+		}  
+		var t = Object(this);  
+		var len = t.length >>> 0;  
+		if (len === 0) {  
+			return -1;	
+		}  
+		var n = 0;	
+		if (arguments.length > 0) {	 
+			n = Number(arguments[1]);  
+			if (n != n) { // shortcut for verifying if it's NaN	 
+				n = 0;	
+			} else if (n != 0 && n != Infinity && n != -Infinity) {	 
+				n = (n > 0 || -1) * Math.floor(Math.abs(n));  
+			}  
+		}  
+		if (n >= len) {	 
+			return -1;	
+		}  
+		var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);  
+		for (; k < len; k++) {	
+			if (k in t && t[k] === searchElement) {	 
+				return k;  
+			}  
+		}  
+		return -1;	
+	}  
+}
 RegExp.escape = function (str) {
 	var specials = new RegExp('[.*+?|\\^$()\\[\\]{}\\\\]', 'g');
 	return str.replace(specials, '\\$&');
@@ -2391,8 +2425,12 @@ function init() {
 		parseData();
 		showReport();
 	} else {
-		// By default show the first file
-		document.getElementById('datafiles').selectedIndex = 0;
+		// By default show dataFilesDefault or the first file
+		if (dataFilesDefault && dataFiles.indexOf(dataFilesDefault) !== -1) {
+			document.getElementById('datafiles').selectedIndex = dataFiles.indexOf(dataFilesDefault);
+		} else {
+			document.getElementById('datafiles').selectedIndex = 0;
+		}
 		loadSelectedFile();
 	}
 
