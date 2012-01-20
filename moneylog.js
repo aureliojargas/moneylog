e2902cabd30e18be3d75dd71556a4c41a6c939d6
Aurelio Jargas <verde@aurelio.net>
2012-01-20 12:17:09 +0000

    tidy: Removing useless spaces at the end of the line.

--- moneylog.js
+++ moneylog.js
@@ -485,37 +485,37 @@ Array.prototype.removePattern = function (patt, n) { // n = number of removes
 };
 
 // https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf
-if (!Array.prototype.indexOf) {  
-	Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {	
-		"use strict";  
-		if (this == null) {	 
-			throw new TypeError();	
-		}  
-		var t = Object(this);  
-		var len = t.length >>> 0;  
-		if (len === 0) {  
-			return -1;	
-		}  
-		var n = 0;	
-		if (arguments.length > 0) {	 
-			n = Number(arguments[1]);  
-			if (n != n) { // shortcut for verifying if it's NaN	 
-				n = 0;	
-			} else if (n != 0 && n != Infinity && n != -Infinity) {	 
-				n = (n > 0 || -1) * Math.floor(Math.abs(n));  
-			}  
-		}  
-		if (n >= len) {	 
-			return -1;	
-		}  
-		var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);  
-		for (; k < len; k++) {	
-			if (k in t && t[k] === searchElement) {	 
-				return k;  
-			}  
-		}  
-		return -1;	
-	}  
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+		"use strict";
+		if (this == null) {
+			throw new TypeError();
+		}
+		var t = Object(this);
+		var len = t.length >>> 0;
+		if (len === 0) {
+			return -1;
+		}
+		var n = 0;
+		if (arguments.length > 0) {
+			n = Number(arguments[1]);
+			if (n != n) { // shortcut for verifying if it's NaN
+				n = 0;
+			} else if (n != 0 && n != Infinity && n != -Infinity) {
+				n = (n > 0 || -1) * Math.floor(Math.abs(n));
+			}
+		}
+		if (n >= len) {
+			return -1;
+		}
+		var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+		for (; k < len; k++) {
+			if (k in t && t[k] === searchElement) {
+				return k;
+			}
+		}
+		return -1;
+	}
 }
 RegExp.escape = function (str) {
 	var specials = new RegExp('[.*+?|\\^$()\\[\\]{}\\\\]', 'g');
@@ -1542,7 +1542,7 @@ function updateTagSummary(theData) {
 	// We have tags?
 	if (tagNames.length || noTagSum !== undefined) {
 		tagNames.sort(sortIgnoreCase);
-		
+
 		// Append no-tag data to the end of the table
 		if (noTagSum !== undefined && showEmptyTagInSummary) {
 			tagNames.push(i18n.labelTagEmpty);
@@ -2402,7 +2402,7 @@ function init() {
 	document.getElementById('help').innerHTML = (
 		wrapme(
 			'b',
-			linkme(i18n.appUrl, appName) + 
+			linkme(i18n.appUrl, appName) +
 			((useDropboxStorage) ? ' by ' + linkme('http://twitter.com/xupisco', '@xupisco') : '') +
 			': ') + i18n.appDescription +
 		array2ul([wrapme('b', i18n.labelReports + ': ') + i18n.helpReports]) +
