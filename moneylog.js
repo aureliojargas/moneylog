adf3f86b152f1eafefb5e9fbaabd3a70cb946ab4
Aurelio Jargas <verde@aurelio.net>
2012-02-22 19:36:24 +0000

    Better explaining in comments the ↻ and ⟳ Unicode chars support.

--- moneylog.js
+++ moneylog.js
@@ -3574,8 +3574,8 @@ function init() {
 			// Hide Edit button. Not functional.
 			document.getElementById('editor-open').style.display = 'none';
 			// Inline mini reload button: [ file.txt ] ↻
-			// Note: "⟳" is better, but few fonts support it :(
-			i18n.labelReload = '<b>↻</b>';
+			// i18n.labelReload = '<b>⟳</b>';  // missing in Opera, Safari Mac/iOS
+			i18n.labelReload = '<b>↻</b>';  // missing in Opera
 			addClass(document.getElementById('source-file-box'), 'mini');
 			addClass(document.getElementById('source-file'), 'mini');
 			addClass(document.getElementById('source-reload'), 'mini');
