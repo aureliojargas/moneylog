45013473662ceebc1cf508c7caf37d4888bc0891
Aurelio Jargas <verde@aurelio.net>
2018-01-25 12:59:11 -0200

    Lint: improve code formatting

--- moneylog.js
+++ moneylog.js
@@ -619,15 +619,15 @@ String.prototype.unacccent = function () {
 	if (!this.match(/[^a-z0-9 ]/)) { // no accented char
 		return this;
 	}
-	return this.replace(
-		/[àáâãäå]/g, 'a').replace(
-		/[èéêë]/g, 'e').replace(
-		/[ìíîï]/g, 'i').replace(
-		/[òóôõö]/g, 'o').replace(
-		/[ùúûü]/g, 'u').replace(
-		/[ýÿ]/g, 'y').replace(
-		/ç/g, 'c').replace(
-		/ñ/g, 'n');
+	return this
+		.replace(/[àáâãäå]/g, 'a')
+		.replace(/[èéêë]/g,   'e')
+		.replace(/[ìíîï]/g,   'i')
+		.replace(/[òóôõö]/g,  'o')
+		.replace(/[ùúûü]/g,   'u')
+		.replace(/[ýÿ]/g,     'y')
+		.replace(/ç/g,        'c')
+		.replace(/ñ/g,        'n');
 };
 
 Array.prototype.clone = function() {
