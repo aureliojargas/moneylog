a4b3496337bf694d1fa5513e7b496822234bd10e
Aurelio Jargas <verde@aurelio.net>
2018-02-02 01:03:10 -0200

    New tool: Array.filter()

--- moneylog.js
+++ moneylog.js
@@ -747,6 +747,36 @@ if (!Array.prototype.indexOf) {
 		return -1;
 	};
 }
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter#polyfill
+/* eslint-disable */
+if (!Array.prototype.filter)
+	Array.prototype.filter = function(func, thisArg) {
+		'use strict';
+		if ( ! ((typeof func === 'Function' || typeof func === 'function') && this) )
+			throw new TypeError();
+
+		var len = this.length >>> 0,
+			res = new Array(len), // preallocate array
+			t = this, c = 0, i = -1;
+		if (thisArg === undefined)
+			while (++i !== len)
+				// checks to see if the key was set
+				if (i in this)
+					if (func(t[i], i, t))
+						res[c++] = t[i];
+		else
+			while (++i !== len)
+				// checks to see if the key was set
+				if (i in this)
+					if (func.call(thisArg, t[i], i, t))
+						res[c++] = t[i];
+
+		res.length = c; // shrink down array to proper size
+		return res;
+	};
+/* eslint-enable */
+
 RegExp.escape = function (str) {
 	var specials = new RegExp('[.*+?|\\^$()\\[\\]{}\\\\]', 'g');
 	return str.replace(specials, '\\$&');
