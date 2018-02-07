af4c4e07dc6e641e0e80ab2161cf87ff9343be1d
Aurelio Jargas <verde@aurelio.net>
2018-02-07 00:21:37 -0200

    Deploy the sample-pt.txt file

--- util/deploy.sh
+++ util/deploy.sh
@@ -12,6 +12,10 @@ deploy_dir="$root/public"
 # Copy main file
 cp moneylog.html "$deploy_dir/index.html"
 
+# Copy sample file (localStorage default text)
+mkdir -p "$deploy_dir/sample"
+cp sample/data-pt.txt "$deploy_dir/sample"
+
 # Copy other files
 cp -r \
   moneylog.css \
